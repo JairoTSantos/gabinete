637f6c1b9b56d09734a5a09e84044ea8c4f8573c
Jairo Santos <107869809+JairoTSantos@users.noreply.github.com>
2024-11-12 20:22:01 -0300

    v1

---
+++ src/views/error.php
@@ -0,0 +1,9 @@
+<link href="css/login.css" rel="stylesheet">
+<div class="container vh-100 d-flex justify-content-center align-items-center">
+    <div class="text-center">
+        <div class="mb-4">
+            <img src="public/img/fatal_error.png" alt="Erro 404" style="width: 20em;" class="img-fluid" />
+        </div>
+        <h4>Ocorreu um erro inesperado na aplicação. <br>Entre em contato com o administrador do sistema para obter suporte.</h4>
+    </div>
+</div>
